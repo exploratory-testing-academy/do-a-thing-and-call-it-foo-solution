37bee88c1b758f2425506b7d2afb50205aff176a
Pyhajarvi Maaret <maaret.pyhajarvi@vaisala.com>
2023-01-29 00:55:20 +0200

    Update conftest

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
 import pytest
 from playwright.sync_api import Browser, Page
 
-@pytest.fixture(scope="session")
-def browser_page(browser: Browser) -> Page:
+@pytest.fixture(scope="function", autouse=False)
+def browser_page(browser: Browser):
     new_page = browser.new_page()
     yield new_page
     new_page.close()
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def open_to_url(browser_page: Page):
     browser_page.goto('https://www.knowtheromans.com/roman-numerals/')
     return browser_page
